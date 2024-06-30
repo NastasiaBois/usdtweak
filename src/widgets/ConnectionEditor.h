e53304294aa71ce8541149fbaa67ccc467bac3d5
cpichard <cpichard@users.noreply.github.com>
2024-06-30 10:16:22 +0100

    remove unused code and comments

--- src/widgets/ConnectionEditor.h
+++ src/widgets/ConnectionEditor.h
@@ -5,10 +5,9 @@
 // Prototype of a connection editor working at the stage level
 //
 PXR_NAMESPACE_USING_DIRECTIVE
+
 void DrawConnectionEditor(const UsdStageRefPtr& prim);
-void DrawConnectionEditor(const UsdPrim& prim);
 
-
-// Experimental
+// Experimental; testing several ways to bring prims in the connection editor
 void CreateSession(const UsdPrim &prim, const std::vector<UsdPrim> &prims);
 void AddPrimsToCurrentSession(const std::vector<UsdPrim> &prims);
